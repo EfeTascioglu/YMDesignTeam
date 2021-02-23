1f8d034f38b65cca15e61e6756ffbf98df400f6b
EfeTascioglu <46626642+EfeTascioglu@users.noreply.github.com>
2021-02-23 13:05:54 -0500

    Adding Sean's Project (Partial - Corrupted)

--- YM Design Team Open World/Assets/Scripts/Bryan's Script/BryanPlayerMovement.cs
+++ YM Design Team Open World/Assets/Scripts/Bryan's Script/BryanPlayerMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class BryanPlayerMovement : MonoBehaviour
 {
     public CharacterController cController;
 
