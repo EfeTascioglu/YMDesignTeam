6b528106a69303a7a009fc758f5d2b9db3525373
Sean-Ye-Student <75680515+Sean-Ye-Student@users.noreply.github.com>
2021-02-24 20:40:59 -0500

    Movement added

--- YM Design Team Open World/Assets/Scripts/Sean's Scripts/PlayerMovement.cs
+++ YM Design Team Open World/Assets/Scripts/Sean's Scripts/PlayerMovement.cs
@@ -4,15 +4,73 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody rb;
+    public GameObject player;
+    public Transform head;
+    public float walk_speed;
+    public float jump_speed;
+    private float XRot;
+    private float YRot;
+    public List<int> max_min_viewX;
+    public List<int> max_min_viewY;
+    public List<int> turn_viewY;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = player.GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            BodyTurnAsHead();
+            rb.velocity = rb.transform.forward * walk_speed;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            BodyTurnAsHead();
+            rb.velocity = rb.transform.forward * -walk_speed;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            BodyTurnAsHead();
+            rb.velocity = rb.transform.right * walk_speed * -1;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            BodyTurnAsHead();
+            rb.velocity = rb.transform.right * walk_speed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jump_speed, rb.velocity.z);
+        }
+
+        if (XRot - Input.GetAxis("Mouse Y") * 4 < max_min_viewX[0] && XRot - Input.GetAxis("Mouse Y") * 4 > max_min_viewX[1])
+        {
+            
+        }
+
+        if (YRot + Input.GetAxis("Mouse X") * 4 < max_min_viewY[0] && YRot + Input.GetAxis("Mouse X") * 4 > max_min_viewY[1])
+        {
+            
+        }
+        XRot -= Input.GetAxis("Mouse Y") * 4;
+        YRot += Input.GetAxis("Mouse X") * 4;
+        head.rotation = Quaternion.Euler(new Vector3(XRot, YRot, head.rotation.z));
+
+    }
+
+    void BodyTurnAsHead()
+    {
+        player.transform.rotation = Quaternion.Euler(new Vector3(player.transform.rotation.x, head.rotation.eulerAngles.y, player.transform.rotation.z));
     }
 }
