509942b8a4ca5550615459bfb378d195c0f13ea2
Sean-Ye-Student <75680515+Sean-Ye-Student@users.noreply.github.com>
2021-02-25 23:25:10 -0500

    Added some more movement

--- YM Design Team Open World/Assets/Scripts/Sean's Scripts/PlayerMovement.cs
+++ YM Design Team Open World/Assets/Scripts/Sean's Scripts/PlayerMovement.cs
@@ -10,11 +10,13 @@ public class PlayerMovement : MonoBehaviour
     public Transform head;
     public float walk_speed;
     public float jump_speed;
-    private float XRot;
-    private float YRot;
-    public List<int> max_min_viewX;
-    public List<int> max_min_viewY;
-    public List<int> turn_viewY;
+    public float XRot;
+    public float YRot;
+    private float YRotBody;
+    public bool SnapToHead;
+    public List<float> max_min_viewX;
+    public List<float> max_min_viewY;
+    public List<float> d_max_min_viewY;
 
     // Start is called before the first frame update
     void Start()
@@ -22,41 +24,47 @@ public class PlayerMovement : MonoBehaviour
         rb = player.GetComponent<Rigidbody>();
         PlayerAnim = player.GetComponent<Animator>();
         Cursor.lockState = CursorLockMode.Locked;
+        d_max_min_viewY[0] = max_min_viewY[0];
+        d_max_min_viewY[1] = max_min_viewY[1];
+        SnapToHead = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 x_z_forward = new Vector3(head.transform.forward.x, 0, head.transform.forward.z);
+        Vector3 x_z_right = new Vector3(head.transform.right.x, 0, head.transform.right.z);
         if (Input.GetKey(KeyCode.W))
         {
-            BodyTurnAsHead();
-            rb.velocity = rb.transform.forward * walk_speed;
+            rb.velocity = x_z_forward * walk_speed;
+
             PlayerAnim.SetTrigger("Walk");
             PlayerAnim.SetBool("Stop", false);
+            SnapToHead = true;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            BodyTurnAsHead();
-            rb.velocity = rb.transform.forward * -walk_speed;
+            rb.velocity = x_z_forward * -walk_speed;
             PlayerAnim.SetTrigger("Walk");
             PlayerAnim.SetBool("Stop", false);
+            SnapToHead = true;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            BodyTurnAsHead();
-            rb.velocity = rb.transform.right * walk_speed * -1;
+            rb.velocity = x_z_right * walk_speed * -1;
             PlayerAnim.SetTrigger("Walk");
             PlayerAnim.SetBool("Stop", false);
+            SnapToHead = true;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            BodyTurnAsHead();
-            rb.velocity = rb.transform.right * walk_speed;
+            rb.velocity = x_z_right * walk_speed;
             PlayerAnim.SetTrigger("Walk");
             PlayerAnim.SetBool("Stop", false);
+            SnapToHead = true;
         }
 
         if (Input.GetKey(KeyCode.LeftShift))
@@ -79,21 +87,35 @@ public class PlayerMovement : MonoBehaviour
 
         if (XRot - Input.GetAxis("Mouse Y") * 4 < max_min_viewX[0] && XRot - Input.GetAxis("Mouse Y") * 4 > max_min_viewX[1])
         {
-            
+            XRot -= Input.GetAxis("Mouse Y") * 4;
         }
 
+        max_min_viewY[0] = d_max_min_viewY[0] + player.transform.localEulerAngles.y;
+        max_min_viewY[1] =  d_max_min_viewY[1] + player.transform.localEulerAngles.y;
+        if (YRot < 0)
+        {
+            YRot = 360 + YRot;
+        }
         if (YRot + Input.GetAxis("Mouse X") * 4 < max_min_viewY[0] && YRot + Input.GetAxis("Mouse X") * 4 > max_min_viewY[1])
         {
-            
+            YRot += Input.GetAxis("Mouse X") * 4;
         }
-        XRot -= Input.GetAxis("Mouse Y") * 4;
-        YRot += Input.GetAxis("Mouse X") * 4;
+        else
+        {
+            YRot += Input.GetAxis("Mouse X") * 4;
+            YRotBody += Input.GetAxis("Mouse X") * 4;
+        }
+
         head.rotation = Quaternion.Euler(new Vector3(XRot, YRot, head.rotation.z));
-
+        if(SnapToHead == false)
+        {
+            player.transform.rotation = Quaternion.Euler(new Vector3(player.transform.rotation.x, YRotBody, player.transform.rotation.z));
+        }
+        else
+        {
+            player.transform.rotation = Quaternion.Euler(new Vector3(player.transform.rotation.x, YRot, player.transform.rotation.z));
+        }
+        SnapToHead = false;
     }
 
-    void BodyTurnAsHead()
-    {
-        player.transform.rotation = Quaternion.Euler(new Vector3(player.transform.rotation.x, head.rotation.eulerAngles.y, player.transform.rotation.z));
-    }
 }
