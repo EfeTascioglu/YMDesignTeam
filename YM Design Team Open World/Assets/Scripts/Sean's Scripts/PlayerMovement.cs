25a40f6e475526894e30145ceedb0222042f7bc3
Sean-Ye-Student <75680515+Sean-Ye-Student@users.noreply.github.com>
2021-03-09 13:56:39 -0500

    Added ui

--- YM Design Team Open World/Assets/Scripts/Sean's Scripts/PlayerMovement.cs
+++ YM Design Team Open World/Assets/Scripts/Sean's Scripts/PlayerMovement.cs
@@ -7,16 +7,26 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody rb;
     private Animator PlayerAnim;
     public GameObject player;
+    public Transform GroundDetector;
     public Transform head;
+    public LayerMask can_jump_on;
     public float walk_speed;
+    private float crouch_speed;
+    private float run_speed;
     public float jump_speed;
-    public float XRot;
-    public float YRot;
+    private float XRot;
+    private float YRot;
     private float YRotBody;
-    public bool SnapToHead;
+    private float RunActivationTime;
+    private float EndActivationTime;
+    private string RunActivationKey;
+    private bool SnapToHead;
+    public int MoveMode;
     public List<float> max_min_viewX;
     public List<float> max_min_viewY;
     public List<float> d_max_min_viewY;
+    private float smoothlerp_start;
+    private float current_body_smoothlerp_v;
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +36,8 @@ public class PlayerMovement : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         d_max_min_viewY[0] = max_min_viewY[0];
         d_max_min_viewY[1] = max_min_viewY[1];
+        crouch_speed = walk_speed * 0.3034f;
+        run_speed = walk_speed * 1.3f;
         SnapToHead = false;
     }
 
@@ -36,57 +48,171 @@ public class PlayerMovement : MonoBehaviour
         Vector3 x_z_right = new Vector3(head.transform.right.x, 0, head.transform.right.z);
         if (Input.GetKey(KeyCode.W))
         {
-            Vector3 new_v = x_z_forward * walk_speed;
+            Vector3 new_v = rb.velocity;
+            if (MoveMode == 2)
+            {
+                new_v = x_z_forward * run_speed;
+                PlayerAnim.SetBool("Stop", true);
+                PlayerAnim.SetTrigger("Run");
+                PlayerAnim.SetBool("Stop", false);
+            }
+            else
+            {
+                if (MoveMode == 1)
+                {
+                    new_v = x_z_forward * walk_speed;
+                    PlayerAnim.SetBool("Stop", true);
+                    PlayerAnim.SetTrigger("Walk");
+                    PlayerAnim.SetBool("Stop", false);
+                }
+                else
+                {
+                    //new_v = x_z_forward * walk_speed;
+                    PlayerAnim.SetBool("Stop", true);
+                    PlayerAnim.SetBool("Crouching", true);
+                    PlayerAnim.SetBool("Stop", false);
+                }
+                
+            }
             rb.velocity = new Vector3(new_v.x, rb.velocity.y, new_v.z);
-
-            PlayerAnim.SetTrigger("Walk");
-            PlayerAnim.SetBool("Stop", false);
             SnapToHead = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (CanRun("W"))
+            {
+                MoveMode = 2;
+            }
+            else
+            {
+                MoveMode = 1;
+                RunActivationKey = "W";
+                RunActivationTime = Time.time;
+            }
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            Vector3 new_v = x_z_forward * -walk_speed;
+            Vector3 new_v = rb.velocity;
+            if (MoveMode == 2)
+            {
+                new_v = x_z_forward * -run_speed;
+                PlayerAnim.SetBool("Stop", true);
+                PlayerAnim.SetTrigger("Run");
+                PlayerAnim.SetBool("Stop", false);
+            }
+            else
+            {
+                if (MoveMode == 1)
+                {
+                    new_v = x_z_forward * -walk_speed;
+                    PlayerAnim.SetBool("Stop", true);
+                    PlayerAnim.SetTrigger("Walk");
+                    PlayerAnim.SetBool("Stop", false);
+                }
+                else
+                {
+                    //new_v = x_z_forward * -walk_speed;
+                    PlayerAnim.SetBool("Stop", true);
+                    PlayerAnim.SetBool("Crouching", true);
+                    PlayerAnim.SetBool("Stop", false);
+                }
+            }
+
             rb.velocity = new Vector3(new_v.x, rb.velocity.y, new_v.z);
-            PlayerAnim.SetTrigger("Walk");
-            PlayerAnim.SetBool("Stop", false);
             SnapToHead = true;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            Vector3 new_v = x_z_right * -walk_speed;
+            Vector3 new_v = rb.velocity;
+            if (MoveMode == 2)
+            {
+                new_v = x_z_right * -run_speed;
+                PlayerAnim.SetBool("Stop", true);
+                PlayerAnim.SetTrigger("Run");
+                PlayerAnim.SetBool("Stop", false);
+            }
+            else
+            {
+                if (MoveMode == 1)
+                {
+                    new_v = x_z_right * -walk_speed;
+                    PlayerAnim.SetBool("Stop", true);
+                    PlayerAnim.SetTrigger("Walk");
+                    PlayerAnim.SetBool("Stop", false);
+                }
+                else
+                {
+                    //new_v = x_z_right * -walk_speed;
+                    PlayerAnim.SetBool("Stop", true);
+                    PlayerAnim.SetBool("Crouching", true);
+                    PlayerAnim.SetBool("Stop", false);
+                }
+            }
+
             rb.velocity = new Vector3(new_v.x, rb.velocity.y, new_v.z);
-            PlayerAnim.SetTrigger("Walk");
-            PlayerAnim.SetBool("Stop", false);
             SnapToHead = true;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            Vector3 new_v = x_z_right * walk_speed;
+            Vector3 new_v = rb.velocity;
+            if (MoveMode == 2)
+            {
+                new_v = x_z_right * run_speed;
+                PlayerAnim.SetBool("Stop", true);
+                PlayerAnim.SetTrigger("Run");
+                PlayerAnim.SetBool("Stop", false);
+            }
+            else
+            {
+                if (MoveMode == 1)
+                {
+                    new_v = x_z_right * walk_speed;
+                    PlayerAnim.SetBool("Stop", true);
+                    PlayerAnim.SetTrigger("Walk");
+                    PlayerAnim.SetBool("Stop", false);
+                }
+                else
+                {
+                    //new_v = x_z_right * walk_speed;
+                    PlayerAnim.SetBool("Stop", true);
+                    PlayerAnim.SetBool("Crouching", true);
+                    PlayerAnim.SetBool("Stop", false);
+                }
+            }
+
             rb.velocity = new Vector3(new_v.x, rb.velocity.y, new_v.z);
-            PlayerAnim.SetTrigger("Walk");
-            PlayerAnim.SetBool("Stop", false);
             SnapToHead = true;
         }
 
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            PlayerAnim.SetBool("Stop", false);
-            PlayerAnim.SetTrigger("Crouch");
-            
+            //MoveMode = 0;
+            //PlayerAnim.SetBool("Stop", false);
         }
-
+        
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = new Vector3(rb.velocity.x, jump_speed, rb.velocity.z);
-            
+            Collider[] collisions = Physics.OverlapSphere(GroundDetector.position, 0.3f, can_jump_on, QueryTriggerInteraction.Ignore);
+            if (collisions.Length > 0)
+            {
+                print(collisions[0]);
+                rb.velocity = new Vector3(rb.velocity.x, jump_speed, rb.velocity.z);
+            }
+                  
         }
 
         if (Input.anyKey == false)
         {
             PlayerAnim.SetBool("Stop", true);
+            if (MoveMode == 2)
+            {
+                EndActivationTime = Time.time;
+            }
+            MoveMode = 1;          
         }
 
         if (XRot - Input.GetAxis("Mouse Y") * 4 < max_min_viewX[0] && XRot - Input.GetAxis("Mouse Y") * 4 > max_min_viewX[1])
@@ -96,6 +222,7 @@ public class PlayerMovement : MonoBehaviour
 
         max_min_viewY[0] = d_max_min_viewY[0] + player.transform.localEulerAngles.y;
         max_min_viewY[1] =  d_max_min_viewY[1] + player.transform.localEulerAngles.y;
+
         if (YRot < 0)
         {
             YRot += 360;
@@ -118,6 +245,7 @@ public class PlayerMovement : MonoBehaviour
                 YRotBody -= 360;
             }
         }
+
         if (YRot + Input.GetAxis("Mouse X") * 4 < max_min_viewY[0] && YRot + Input.GetAxis("Mouse X") * 4 > max_min_viewY[1])
         {
             YRot += Input.GetAxis("Mouse X") * 4;
@@ -127,15 +255,40 @@ public class PlayerMovement : MonoBehaviour
             YRot += Input.GetAxis("Mouse X") * 4;
             YRotBody += Input.GetAxis("Mouse X") * 4;
         }
-        print(YRot);
-        print(YRotBody);
+
         head.rotation = Quaternion.Euler(new Vector3(XRot, YRot, head.rotation.z));
-        if(SnapToHead == true)
+        //Debug.Log(YRot.ToString() + " " + YRotBody.ToString());
+
+        if (SnapToHead == true)
         {
-            YRotBody = YRot;
+            if (smoothlerp_start == 0)
+            {
+                smoothlerp_start = Time.time;
+            }
+
+            YRotBody = Mathf.SmoothDampAngle(YRotBody, YRot, ref current_body_smoothlerp_v, 1, 100, (Time.time - smoothlerp_start));
         }
-        player.transform.rotation = Quaternion.Euler(new Vector3(player.transform.rotation.x, YRotBody, player.transform.rotation.z));
+        else
+        {
+            smoothlerp_start = 0;
+        }
+
+        player.transform.rotation = Quaternion.Euler(new Vector3(player.transform.rotation.x, (float)YRotBody, player.transform.rotation.z));
         SnapToHead = false;
     }
 
+    bool CanRun(string key_pressed)
+    {
+        if (RunActivationKey == key_pressed && Time.time - RunActivationTime < 0.25 && Time.time - EndActivationTime > 0.25)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(GroundDetector.position, 0.3f);
+    }
 }
