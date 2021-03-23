d4f7a615255c156c9282066bf118486c6a52308b
Sean-Ye-Student <75680515+Sean-Ye-Student@users.noreply.github.com>
2021-03-23 19:52:01 -0400

    more stuff

---
+++ YM Design Team Open World/Assets/Scripts/Sean's Scripts/WaterGenerator.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterGenerator : MonoBehaviour
+{
+    public int depth;
+    public int length;
+    public int width;
+
+    public float scale;
+    public float offsetX;
+    public float offsetY;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Terrain terrain = GetComponent<Terrain>();
+        terrain.terrainData = GenerateTerrain(terrain.terrainData);
+
+        offsetX += speed;
+        offsetY += speed;
+
+        if (offsetX > 1000000000)
+        {
+            offsetX = 0;
+        }
+
+        if (offsetY > 1000000000)
+        {
+            offsetY = 0;
+        }
+    }
+
+    TerrainData GenerateTerrain(TerrainData terrainData)
+    {
+        terrainData.heightmapResolution = width + 1;
+        terrainData.size = new Vector3(length, depth, width);
+        terrainData.SetHeights(0, 0, GenerateHeights());
+        return terrainData;
+    }
+
+    float[,] GenerateHeights()
+    {
+        float[,] heights = new float[length, width];
+        for (int x = 0; x < length; x++)
+        {
+            for (int y = 0; y < width; y++)
+            {
+                heights[x, y] = CalculateHeight(x, y);
+            }
+        }
+
+        return heights;
+    }
+
+    float CalculateHeight(int x, int y)
+    {
+        float xCoords = (float)x / length * scale + offsetX;
+        float yCoords = (float)y / width * scale + offsetY;
+
+        return Mathf.PerlinNoise(xCoords, yCoords);
+    }
+}
