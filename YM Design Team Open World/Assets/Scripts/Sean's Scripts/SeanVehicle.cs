f257e2c5ea04a57ccb0ae651ae0034c37b25db9f
Sean-Ye-Student <75680515+Sean-Ye-Student@users.noreply.github.com>
2021-05-12 18:37:47 -0400

    Driving

--- YM Design Team Open World/Assets/Scripts/Sean's Scripts/SeanVehicle.cs
+++ YM Design Team Open World/Assets/Scripts/Sean's Scripts/SeanVehicle.cs
@@ -2,97 +2,152 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable] //all this does is make this class able to be edited in the inspector
+public class AxleInfo
+{
+    public WheelCollider left;
+    public WheelCollider right;
+    public bool can_steer;
+    public bool powered;
+}
 public class SeanVehicle : MonoBehaviour
 {
-    public float speed;
-    private Rigidbody rb;
-    public GameObject Armature;
-    public GameObject Vehicle;
-    public Transform[] FrontTires;
+    //public GameObject Armature;
+    //public GameObject Vehicle;
+
+    public List<AxleInfo> AxleInfo;
     public float MaxAngle;
-    
+    public float MaxSpeed;
+    public float current_angle;
+    public float current_speed;
+
+    public float acceleration;
+    public float turn_speed;
+    public bool brakes;
+
+    private GameObject armature;
+    private Rigidbody rb;
+    public Transform car_origin;
+
+    //public BoxCollider collider;
+
     // Start is called before the first frame update
     void Start()
     {
-        rb = Vehicle.GetComponent<Rigidbody>();
+       
     }
 
     // Update is called once per frame
-    void Update()
+    public void FixedUpdate()
     {
-        rb.velocity = new Vector3(0, 0, 0);
-        rb.angularVelocity = new Vector3(0, 0, 0);
-        if (Input.GetMouseButtonDown(2))
+        print(armature);
+        print(rb);
+        
+        if (armature == null)
         {
-            Armature = FindObjectOfType<MinecraftPlayerMovement>().gameObject;
-            
-        }
+         
 
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            Armature.transform.localScale = new Vector3(1, 1, 1);
-            Armature.transform.position = Vehicle.transform.position + new Vector3(0, 2, 0);
-            Armature.GetComponent<CapsuleCollider>().enabled = true;
-            Armature = null;
-        }
-
-        if(Armature != null)
-        {
-            Armature.GetComponent<CapsuleCollider>().enabled = false;
-            Armature.transform.position = Vehicle.transform.position;
-            Armature.transform.localScale = new Vector3(0, 0, 0);
-
-            if (Input.GetKey(KeyCode.A))
-            {
-                for (int i = 0; i < FrontTires.Length; i++)
-                {
-                    FrontTires[i].localEulerAngles = new Vector3(0, -MaxAngle, 0);
-                }
-            }
-            else
-            {
-                if (Input.GetKey(KeyCode.D))
-                {
-                    for (int i = 0; i < FrontTires.Length; i++)
-                    {
-                        FrontTires[i].localEulerAngles = new Vector3(0, MaxAngle, 0);
-                    }
-                }
-                
-            }
-
-
-            if (Input.GetKey(KeyCode.W))
-            {
-                for (int i = 0; i < FrontTires.Length; i++)
-                {
-                    Rigidbody ftrb = FrontTires[i].GetComponent<Rigidbody>();
-                    ftrb.angularVelocity = new Vector3(speed, 0, 0);
-                }
-                //rb.velocity = new Vector3(FrontTires[0].transform.forward.x * speed, rb.velocity.y, FrontTires[0].transform.forward.z * speed);
-            }
-            else
-            {
-                if (Input.GetKey(KeyCode.S))
-                {
-                    for (int i = 0; i < FrontTires.Length; i++)
-                    {
-                        Rigidbody ftrb = FrontTires[i].GetComponent<Rigidbody>();
-                        ftrb.angularVelocity = FrontTires[i].transform.right * -speed;
-                    }
-                    //rb.velocity = new Vector3(FrontTires[0].transform.forward.x * -speed, rb.velocity.y, FrontTires[0].transform.forward.z * -speed);
-                }
-            }
-            
+            return;
         }
         else
         {
-            for (int i = 0; i < FrontTires.Length; i++)
+            armature.transform.localScale = new Vector3(0, 0, 0);
+            armature.transform.position = car_origin.position;
+
+            if(rb != null)
             {
-                Rigidbody ftrb = FrontTires[i].GetComponent<Rigidbody>();
-                ftrb.angularVelocity = FrontTires[i].transform.right * speed;
+                rb.useGravity = false;
+            }
+            
+        }
+        brakes = false;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            armature.transform.position = car_origin.position + (car_origin.right * 1.5f);
+            armature.transform.localScale = new Vector3(1, 1, 1);
+            if (rb != null)
+            {
+                rb.useGravity = true;
+                rb = null;
+            }
+            armature = null;
+            brakes = true;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            current_speed = MaxSpeed;
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.S))
+            {
+                current_speed = -MaxSpeed;
+
+            }
+            else
+            {
+                brakes = true;
             }
         }
-      
+        
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            current_angle = MaxAngle;
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.A))
+            {                
+                current_angle = -MaxAngle;
+            }
+            else
+            {
+                current_angle = 0;
+            }
+        }
+
+        foreach (var axle in AxleInfo)
+        {
+            if (axle.can_steer)
+            {
+                axle.left.steerAngle = current_angle;
+                axle.right.steerAngle = current_angle;
+
+                axle.left.gameObject.transform.localRotation = Quaternion.Euler(0, current_angle, 0);
+                axle.right.gameObject.transform.localRotation = Quaternion.Euler(0, current_angle, 0);
+            }
+
+            if (axle.powered)
+            {
+                axle.left.motorTorque = current_speed;
+                axle.right.motorTorque = current_speed;
+            }
+
+            axle.left.gameObject.transform.localRotation = Quaternion.Euler(0,  0, axle.left.rpm / 240 * Time.deltaTime);
+            axle.right.gameObject.transform.localRotation = Quaternion.Euler(0, 0, axle.right.rpm / 240 * Time.deltaTime);
+
+            if (brakes)
+            {
+                axle.left.wheelDampingRate = 100;
+                axle.right.wheelDampingRate = 100;
+            }
+            else
+            {
+                axle.left.wheelDampingRate = 0.25f;
+                axle.right.wheelDampingRate = 0.25f;
+            }
+        }
     }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.GetComponent<MinecraftPlayerMovement>())
+        {
+            armature = other.gameObject;
+            rb = armature.GetComponent<Rigidbody>();
+        }
+    }
+
 }
